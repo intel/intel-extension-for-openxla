c36ddc9f150d62de995b18de87bfb21265ca2d8c
Lu Teng <teng.lu@intel.com>
2024-09-05 02:42:03 -0700

    Update T5.

--- example/t5/quick_start.sh
+++ example/t5/quick_start.sh
@@ -65,7 +65,7 @@ echo $MODEL_PATH
 echo "Please make sure ${NUM_GPUS} is the number of visible CUDA devices you have"
 
 # Setting XLA flags
-export XLA_FLAGS="--xla_gpu_simplify_all_fp_conversions --xla_gpu_all_reduce_combine_threshold_bytes=136314880 ${XLA_FLAGS}"
+export XLA_FLAGS="--xla_allow_excess_precision --xla_gpu_all_reduce_combine_threshold_bytes=136314880 ${XLA_FLAGS}"
 
 
 PREFIX=""
