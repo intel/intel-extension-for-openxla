68b2112b3466d0fcb111369799246d39281c7452
ellie-jan <110058044+ellie-jan@users.noreply.github.com>
2025-05-28 15:49:56 -0700

    Resolve dependency conflicts for Flan T5 (#604)
    Co-authored-by: ellie.jan <ejan@mlp-prod-clx-4892.jf.intel.com>

--- example/t5/install_xpu.sh
+++ example/t5/install_xpu.sh
@@ -7,8 +7,6 @@ git apply ../patch/t5.patch
 
 ln -s /usr/local/bin/pip /usr/bin/pip
 pip uninstall tensorflow-metadata numba cudf -y
-pip uninstall tensorflow -y
-pip install tensorflow==2.18.0
 
 conda install libstdcxx-ng -c conda-forge -y
 
@@ -19,5 +17,9 @@ pip install -e .
 pip install orbax-checkpoint==0.3.2
 pip install zstandard==0.21.0
 pip install jsonlines==3.1.0
+
+pip uninstall tensorflow tensorflow-cpu tensorflow-text -y
+#TensorFlow Text will auto-install the right TensorFlow version as its dependency
+pip install tensorflow-text==2.18.1
 
 popd
